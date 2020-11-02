a5897835bbe1c8868dda18d99b312270634a6dac
Taromaru <48397556+Taromaruu@users.noreply.github.com>
2020-11-02 15:20:41 -0500

    Seperated basic.py into multiple files. Added logs in case Doggoscript has a exception.

--- doggoscript.py
+++ doggoscript.py
@@ -1,8 +1,49 @@
 import basic
-import sys
+import sys, os
+import traceback
+import datetime
+import random
+import platform
+import winreg
+
+crash_messages = ["Uh oh...", "Looks like doggo has broke your hopes and dreams", "Boom.", "Look what you have done!", "Pog"]
 
 print("Doggoscript 0.1.5")
 print("Docs: https://docs.doggoscript.ml")
+
+def generate_traceback():
+    rand = random.choice(crash_messages)
+    Registry = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
+    with winreg.OpenKey(Registry, "SOFTWARE\Doggo\Doggoscript") as k:
+        try:
+            ds_ver = winreg.QueryValueEx(k, "Version")[0]
+        except FileNotFoundError:
+            ds_ver = "Key not found in reg"
+    
+    x = datetime.datetime.now()
+    y = x.strftime("%B-%d-%Y %I-%M-%S %p")
+    if not os.path.isdir("crash_logs"):
+        os.mkdir("crash_logs")
+    with open(f"crash_logs/{y}.log", "w", encoding="utf-8") as f:
+        f.write(
+f"""--- Begining of crash report ---
+// {rand}
+
+Time: {x}
+Description: {e}
+
+Full Traceback:\n\n{traceback.format_exc()}
+
+--- System Info ---
+Doggoscript Version: {ds_ver}
+Python Version: {platform.python_version()}
+
+Node Name: {platform.node()}
+Platform: {platform.platform()} {platform.machine()}
+Processor: {platform.processor()}
+Architecture: {" ".join(platform.architecture())}
+""")
+    print(f"\nDoggoscript has ran into a exception! Log {y}.log has been created.")
 
 while True:
     try:
@@ -23,7 +64,12 @@ while True:
             break
         except EOFError:
             break
-        result, error = basic.run("<stdin>", text)
+
+        try:
+            result, error = basic.run("<stdin>", text)
+        except BaseException as e:
+            generate_traceback()
+            break
 
         if error:
             print(error.as_string())
@@ -32,3 +78,6 @@ while True:
                 print(repr(result.elements[0]))
             else:
                 print(repr(result))
+    except BaseException as e:
+        generate_traceback()
+        break
---
+++ doggoscript/__init__.py
@@ -0,0 +1,103 @@
+import string
+
+DIGITS = '0123456789'
+LETTERS = string.ascii_letters
+LETTERS_DIGITS = LETTERS + DIGITS
+
+class Position:
+    def __init__(self, idx, ln, col, fn, ftxt):
+        self.idx = idx
+        self.ln = ln
+        self.col = col
+        self.fn = fn
+        self.ftxt = ftxt
+
+    def advance(self, current_char=None):
+        self.idx += 1
+        self.col += 1
+
+        if current_char == '\n':
+            self.ln += 1
+            self.col = 0
+
+        return self
+
+    def copy(self):
+        return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)
+
+class RTResult:
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.value = None
+        self.error = None
+        self.func_return_value = None
+        self.loop_should_continue = False
+        self.loop_should_break = False
+
+    def register(self, res):
+        self.error = res.error
+        self.func_return_value = res.func_return_value
+        self.loop_should_continue = res.loop_should_continue
+        self.loop_should_break = res.loop_should_break
+        return res.value
+
+    def success(self, value):
+        self.reset()
+        self.value = value
+        return self
+
+    def success_return(self, value):
+        self.reset()
+        self.func_return_value = value
+        return self
+
+    def success_continue(self):
+        self.reset()
+        self.loop_should_continue = True
+        return self
+
+    def success_break(self):
+        self.reset()
+        self.loop_should_break = True
+        return self
+
+    def failure(self, error):
+        self.reset()
+        self.error = error
+        return self
+
+    def should_return(self):
+        # Note: this will allow you to continue and break outside the current function
+        return (
+            self.error or
+            self.func_return_value or
+            self.loop_should_continue or
+            self.loop_should_break
+        )
+
+class Context:
+    def __init__(self, display_name, parent=None, parent_entry_pos=None):
+        self.display_name = display_name
+        self.parent = parent
+        self.parent_entry_pos = parent_entry_pos
+        self.symbol_table = None
+
+class SymbolTable:
+    def __init__(self, parent=None):
+        self.symbols = {}
+        self.parent = parent
+
+    def get(self, name):
+        value = self.symbols.get(name, None)
+        if value == None and self.parent:
+            return self.parent.get(name)
+        return value
+
+    def set(self, name, value):
+        self.symbols[name] = value
+
+    def remove(self, name):
+        del self.symbols[name]
+
